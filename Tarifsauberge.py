475743e0232d2797b79bf190f5881b6e0587c15f
Julie Boutelet <julie.boutelet@gmail.com>
2022-07-02 11:49:23 +0200

    Add files via upload

---
+++ Tarifsauberge.py
@@ -0,0 +1,14 @@
+age = int(input())
+
+if age == 60:
+    print("0")
+elif age < 10:
+    print("5")
+else :
+    bagage = int(input())
+    if bagage >= 20:
+        print("40")
+    else:
+        print("30")
+
+
